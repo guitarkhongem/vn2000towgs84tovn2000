ba108588a1243351bac73e0c9d4be2823ef4571b
guitarkhongem <guitarkhongem@gmail.com>
2025-04-25 14:34:17 +0700

    Update app.py

--- vn2000towgs84tovn2000/app.py
+++ vn2000towgs84tovn2000/app.py
@@ -1,16 +1,14 @@
-
 import streamlit as st
+st.set_page_config(page_title="VN2000 ⇄ WGS84 Converter", layout="wide")
+
+# Các import khác
 import analytics
 import pandas as pd
-# ==== CẤU HÌNH GIAO DIỆN ====
-st.set_page_config(page_title="VN2000 ⇄ WGS84 Converter", layout="wide")
 import math
 import re
 import folium
 from streamlit_folium import st_folium
 from functions import vn2000_to_wgs84_baibao, wgs84_to_vn2000_baibao
-analytics.log_visit()
-# Ghi lượt truy cập (có thể thêm IP nếu lấy được)
 analytics.log_visit()
 
 # Tăng like nếu nhấn nút
